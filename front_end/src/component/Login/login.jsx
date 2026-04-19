4a1310ebdb11ea80870318db2ab6527af52ef923
Abhishek <abisheksamuditha552@gmail.com>
2026-04-19 12:19:14 +0530

    change the login page design

--- front_end/src/component/Login/login.jsx
+++ front_end/src/component/Login/login.jsx
@@ -1,14 +1,14 @@
 
 import React from 'react'
 import './login.css'
-import user_icon from '../../assets/drafts_29dp_000000_FILL0_wght400_GRAD0_opsz24.png'
-import password_icon from '../../assets/password_29dp_000000_FILL0_wght400_GRAD0_opsz24.svg'
+import user_icon from '../../assets/password_29dp_000000_FILL0_wght400_GRAD0_opsz24.svg'
+import password_icon from '../../assets/drafts_29dp_000000_FILL0_wght400_GRAD0_opsz24.png'
 import person_add_icon from '../../assets/person_29dp_000000_FILL0_wght400_GRAD0_opsz24.svg'
 
 
 const Login = ({ onClose }) => {
   const [action, setAction] = React.useState('Sign up');
-
+password_29dp_000000_FILL0_wght400_GRAD0_opsz24.svg
   return (
     <div className="container">
       <div className="header">
